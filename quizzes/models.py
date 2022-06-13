c2da9bc3f64da0f4b9bdbf5198b75a055aaf0607
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-13 23:03:49 +0100

    Define initial topic/word models with simple fields

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# most essential model fields to get started
+class Topic(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=100, blank=True)
+    is_hidden = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Word(models.Model):
+    origin = models.CharField(max_length=100)
+    target = models.CharField(max_length=100)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='words')
+    date_created = models.DateTimeField(auto_now_add=True)
