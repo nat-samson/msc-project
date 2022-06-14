ce87b9deb08649dc80ff88dc2508e171aca9ad08
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-14 21:19:36 +0100

    Improved home view, added home html template

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,24 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-# dummy data, will get data from model later
-topics = [
-    {
-        'name': 'My Family',
-        'words': {'Mother': 'die Mutter', 'Father': 'der Vater'}
-    },
-    {
-        'name': 'Going On Holiday',
-        'words': {'Vacation': 'der Urlaub', 'Beach': 'der Strand'}
-    },
-    {
-        'name': 'Animals',
-        'words': {'Dog': 'der Hund', 'Cat': 'die Katze'}
-    }
-]
+from quizzes.models import Topic
 
 
 # temporary basic home view
 def home(request):
-    context = [topic.get('name') for topic in topics]
-    return HttpResponse('<br>'.join(context))
+    context = {
+        'topics': Topic.objects.all()
+    }
+    return render(request, 'quizzes/home.html', context)
