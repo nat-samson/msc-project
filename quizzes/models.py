d93c352a959286628dff8ab98068b0082a3eb00f
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-14 20:48:02 +0100

    Added __str__ for models, model tests

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # most essential model fields to get started
@@ -8,9 +9,15 @@ class Topic(models.Model):
     is_hidden = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Word(models.Model):
     origin = models.CharField(max_length=100)
     target = models.CharField(max_length=100)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='words')
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.origin}: {self.target}'
--- quizzes/tests.py
+++ quizzes/tests.py
@@ -1,8 +1,50 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .models import Topic, Word
+from .views import home
 
 
 class HomeTests(TestCase):
     def test_home_view_status(self):
         url = reverse('home')
-        self.assertEquals(self.client.get(url).status_code, 200)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_view_url(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
+
+
+class TopicModelTests(TestCase):
+    def setUp(self):
+        Topic.objects.create(name='Animals', description='Practice your German words for Animals.')
+        Topic.objects.create(name='Colours', description='All the colours of des Regenbogens.')
+
+    def test_topics_str(self):
+        # test __str__ for Topics
+        animals = Topic.objects.get(name="Animals")
+        colours = Topic.objects.get(name="Colours")
+        self.assertEqual(str(animals), 'Animals')
+        self.assertEqual(str(colours), 'Colours')
+
+
+class WordModelTests(TestCase):
+    def setUp(self):
+        Topic.objects.create(name='Animals', description='Practice your German words for Animals.')
+        animals = Topic.objects.get(name='Animals')
+        Word.objects.create(origin='Mouse', target='die Maus', topic=animals)
+        Word.objects.create(origin='Fish', target='der Fisch', topic=animals)
+
+    def test_words_str(self):
+        # test __str__ for Words
+        mouse = Word.objects.get(origin="Mouse")
+        fish = Word.objects.get(origin="Fish")
+        self.assertEqual(str(mouse), 'Mouse: die Maus')
+        self.assertEqual(str(fish), 'Fish: der Fisch')
+
+    def test_words_via_origin_target(self):
+        # test that origin and target both lead to correct Word
+        mouse = Word.objects.get(origin="Mouse")
+        mouse_via_maus = Word.objects.get(target='die Maus')
+        self.assertEqual(mouse, mouse_via_maus)
