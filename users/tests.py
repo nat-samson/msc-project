994fd65375287f11779dcbfb68f960b9bb34cc22
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-18 15:56:12 +0100

    Start user creation functionality

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import register
 
-# Create your tests here.
+
+class RegisterTests(TestCase):
+    def test_register_vew_status(self):
+        url = reverse('register')
+        response = self.client.get(url)
+        self.assertEquals(200, response.status_code)
+
+    def test_register_view_url(self):
+        view = resolve('/register/')
+        self.assertEquals(view.func, register)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
+
 # Create your views here.
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
