c1e4c94334a082211077939a9ce7ea1f70c1bd29
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-18 17:26:30 +0100

    Add logout functionality

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
+# alternatively to going to post-logout page, could simply redirect
+# LOGOUT_REDIRECT_URL = 'home'
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', users_views.register, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('quizzes.urls')),
 ]
