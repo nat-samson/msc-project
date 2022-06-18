15dc559402f03762cc4447072c5ab680077a0760
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-18 15:06:15 +0100

    Tidied up tests, standardised expected/actual ordering

--- quizzes/tests.py
+++ quizzes/tests.py
@@ -1,45 +1,40 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-
 from .models import Topic, Word
 from .views import home
 
 
 class HomeTests(TestCase):
-    def test_home_view_status(self):
+    def setUp(self):
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_home_view_status(self):
+        self.assertEquals(200, self.response.status_code)
 
     def test_home_view_url(self):
         view = resolve('/')
-        self.assertEquals(view.func, home)
+        self.assertEquals(home, view.func)
 
     def test_home_template_name(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertTemplateUsed(response, 'quizzes/home.html')
+        self.assertTemplateUsed(self.response, 'quizzes/home.html')
 
 
 class TopicModelTests(TestCase):
     def setUp(self):
-        animals = Topic.objects.create(name='Animals', description='Practice your German words for Animals.')
-        Topic.objects.create(name='Colours', description='All the colours of des Regenbogens.')
-        Word.objects.create(origin='Mouse', target='die Maus').topics.add(animals)
-        Word.objects.create(origin='Fish', target='der Fisch').topics.add(animals)
+        self.animals = Topic.objects.create(name='Animals', description='Practice your German words for Animals.')
+        self.colours = Topic.objects.create(name='Colours', description='All the colours of des Regenbogens.')
+        Word.objects.create(origin='Mouse', target='die Maus').topics.add(self.animals)
+        Word.objects.create(origin='Fish', target='der Fisch').topics.add(self.animals)
 
     def test_topics_str(self):
         # test __str__ for Topics
-        animals = Topic.objects.get(name="Animals")
-        colours = Topic.objects.get(name="Colours")
-        self.assertEqual(str(animals), 'Animals')
-        self.assertEqual(str(colours), 'Colours')
+        self.assertEqual('Animals', str(self.animals))
+        self.assertEqual('Colours', str(self.colours))
 
     def test_topic_word_count(self):
-        animals = Topic.objects.get(name="Animals")
-        colours = Topic.objects.get(name="Colours")
-        self.assertEqual(animals.words.count(), 2)
-        self.assertEqual(colours.words.count(), 0)
+        self.assertEqual(2, self.animals.words.count())
+        self.assertEqual(0, self.colours.words.count())
 
 
 class WordModelTests(TestCase):
@@ -47,16 +42,15 @@ class WordModelTests(TestCase):
         animals = Topic.objects.create(name='Animals', description='Practice your German words for Animals.')
         Word.objects.create(origin='Mouse', target='die Maus').topics.add(animals)
         Word.objects.create(origin='Fish', target='der Fisch').topics.add(animals)
+        self.mouse = Word.objects.get(origin="Mouse")
+        self.fish = Word.objects.get(origin="Fish")
 
     def test_words_str(self):
         # test __str__ for Words
-        mouse = Word.objects.get(origin="Mouse")
-        fish = Word.objects.get(origin="Fish")
-        self.assertEqual(str(mouse), 'Mouse -> die Maus')
-        self.assertEqual(str(fish), 'Fish -> der Fisch')
+        self.assertEqual('Mouse -> die Maus', str(self.mouse))
+        self.assertEqual('Fish -> der Fisch', str(self.fish))
 
     def test_words_via_origin_target(self):
         # test that origin and target both lead to correct Word
-        mouse = Word.objects.get(origin="Mouse")
         mouse_via_maus = Word.objects.get(target='die Maus')
-        self.assertEqual(mouse, mouse_via_maus)
+        self.assertEqual(self.mouse, mouse_via_maus)
