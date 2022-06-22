623d456145c8861f6beb19a078f5b5c81a912eeb
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-22 20:55:13 +0100

    Add StudentRegistrationForm and CustomUserCreationForm

--- users/views.py
+++ users/views.py
@@ -15,6 +15,16 @@ class StudentRegisterView(CreateView):
     form_class = StudentRegistrationForm
     template_name = 'users/register_form.html'
 
+    def get_context_data(self, **kwargs):
+        # add student to the context
+        context = super().get_context_data(**kwargs)
+        context['student_or_teacher'] = 'student'
+        return context
+
+    def form_valid(self, form):
+        user = form.save()
+        return redirect('login')
+
 
 class TeacherRegisterView(CreateView):
     pass
