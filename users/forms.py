e5901efc6c3497d2434b77699c59a130354c5110
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-22 20:58:28 +0100

    correction: Add StudentRegistrationForm and CustomUserCreationForm

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,41 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from users.models import User
+from django.db import transaction
+
+from users.models import User, StudentProfile
 
 
 # add email as an additional field to the register form
-class UserRegistrationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
+    # add fields relevant to students and teachers that are in the Custom User model
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=32)
+    last_name = forms.CharField(max_length=32)
+
+    class Meta:
+        abstract = True
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class StudentRegistrationForm(CustomUserCreationForm):
+    # add fields relevant only to students
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+
+        # auto-create the connected student profile
+        student = StudentProfile.objects.create(user=user)
+
+        # here's where you'd add extra info to the student profile
+
+        return user
+
+
+class TeacherRegistrationForm(CustomUserCreationForm):
     email = forms.EmailField()
 
     class Meta:
