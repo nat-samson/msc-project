5d2edf4e7b7d9d44aacd0563d9941139dd1dd042
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-23 18:16:39 +0100

    Add WordScore model, M2M relationship for User and Word

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,10 +1,14 @@
+from datetime import date
+
 from django.db import models
-from django.contrib.auth.models import User
+
+from users.models import User
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=32, unique=True)
-    description = models.CharField(max_length=100, blank=True)
+    long_desc = models.CharField(max_length=255, blank=True)
+    short_desc = models.CharField(max_length=10, default='❓🧠❓')
     is_hidden = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
 
@@ -20,6 +24,7 @@ class Word(models.Model):
     origin = models.CharField(max_length=100, unique=True)
     target = models.CharField(max_length=100, unique=True)
     topics = models.ManyToManyField(Topic, related_name='words')
+    students = models.ManyToManyField(User, through='WordScore', through_fields=('word', 'student'), related_name='words')
     date_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -27,3 +32,16 @@ class Word(models.Model):
 
     def __str__(self):
         return f'{self.origin} -> {self.target}'
+
+
+class WordScore(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    consecutive_correct = models.PositiveSmallIntegerField(default=0)
+    times_seen = models.PositiveSmallIntegerField(default=0)
+    times_correct = models.PositiveSmallIntegerField(default=0)
+    next_review = models.DateField(default=date.today)
+    score = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('word', 'student')
