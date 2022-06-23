c70ed5217d9e64f98402487b38f1c85285c95e28
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-06-23 20:29:49 +0100

    Update TopicDetailView to show the current user's word scores

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,7 +1,8 @@
+from django.db.models import FilteredRelation, Q
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from quizzes.models import Topic
+from quizzes.models import Topic, Word
 
 
 # temporary basic home view - INACTIVE
@@ -18,9 +19,19 @@ class TopicListView(ListView):
     context_object_name = 'topics'
     ordering = ['date_created']
     #paginate_by = 6  # TODO: Pagination
+    # TODO: only show topics that are 'quizzable', i.e. have enough words, aren't hidden
 
 
 class TopicDetailView(DetailView):
     # model tells the view which model to use to create the detail view
     model = Topic
+    fields = ['name', 'long_desc', 'short_desc']
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # get the current user's word scores for the given topic
+        context['words_with_scores'] = Word.objects.filter(topics=context['topic']).annotate(
+            score=FilteredRelation('wordscore', condition=Q(wordscore__student=self.request.user)),
+        ).values('origin', 'target', 'score')
+        return context
