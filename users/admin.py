eec075511e934beb979f025b5547a4c8368c1a8e
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-07-03 13:50:10 +0100

    Add User model to Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+            }
+
+        if (
+                not is_superuser
+                and obj is not None
+                and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for field in disabled_fields:
+            if field in form.base_fields:
+                form.base_fields[field].disabled = True
+
+        return form
