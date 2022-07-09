15e3e4125a1b855be3144bbcf6a38a83abf83bee
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-07-09 13:48:20 +0100

    Added more tests for quiz view

--- quizzes/tests.py
+++ quizzes/tests.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+
 from django.test import TestCase
 from django.urls import reverse, resolve
 
@@ -114,8 +117,26 @@ class WordScoreModelTests(TestCase):
 class QuizTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.today = datetime.date.today()
+        cls.tomorrow = cls.today + datetime.timedelta(days=1)
+
+        # Set up TOPIC and WORD
         animals = Topic.objects.create(name='Animals', long_desc='Practice your German words for Animals.')
+        cls.mouse = Word.objects.create(origin='Mouse', target='die Maus')
+        cls.cat = Word.objects.create(origin='Cat', target='die Katze')
+        dog = Word.objects.create(origin='Dog', target='der Hund')
+        cls.mouse.topics.add(animals)
+        cls.cat.topics.add(animals)
+        dog.topics.add(animals)
+
+        # Set up USER (a student)
         cls.student = User.objects.create_user(username='test_user', password='test_user1234', is_student=True)
+
+        # Set up WORDSCORE
+        # nb no WordScore is created in advance for mouse
+        cls.dog_score = WordScore.objects.create(word=dog, student=cls.student, times_seen=10,
+                                                 times_correct=5, consecutive_correct=5)
+
         cls.path = f'/quiz/{animals.pk}/'
 
     def test_quiz_view_status(self):
@@ -136,6 +157,78 @@ class QuizTests(TestCase):
 
         # form must have no extra inputs beyond those specified above
         self.assertContains(response, '<input type=', 2)
+
+    def test_quiz_results_question_correct(self):
+        # No WordScore exists for this word / user combination
+        self.client.force_login(self.student)
+        quiz_results = {
+            '1': True
+        }
+        results = json.dumps(quiz_results)
+
+        self.client.post(self.path, {'results': results})
+
+        # check score has been saved in database
+        word_score = WordScore.objects.get(word_id=1, student=self.student)
+        self.assertEquals(1, word_score.consecutive_correct)
+        self.assertEquals(1, word_score.times_seen)
+        self.assertEquals(1, word_score.times_correct)
+        self.assertEquals(self.tomorrow, word_score.next_review)
+
+    def test_quiz_results_question_incorrect(self):
+        # No WordScore exists for this word / user combination
+        self.client.force_login(self.student)
+        quiz_results = {
+            '1': False
+        }
+        results = json.dumps(quiz_results)
+
+        self.client.post(self.path, {'results': results})
+
+        # check score has been saved in database
+        word_score = WordScore.objects.get(word_id=1, student=self.student)
+        self.assertEquals(0, word_score.consecutive_correct)
+        self.assertEquals(1, word_score.times_seen)
+        self.assertEquals(0, word_score.times_correct)
+        self.assertEquals(self.today, word_score.next_review)
+
+    def test_quiz_results_not_due_review_correct(self):
+        # cat_score is due for review tomorrow, so this is an optional extra revision session
+        # getting it correct DOES NOT affect the word_score in such situations
+        self.client.force_login(self.student)
+        cat_score = WordScore.objects.create(word=self.cat, student=self.student, times_correct=3, times_seen=5,
+                                             next_review=self.tomorrow, consecutive_correct=3)
+        quiz_results = {
+            '2': True
+        }
+        results = json.dumps(quiz_results)
+        self.client.post(self.path, {'results': results})
+
+        # check that nothing has changed with the WordScore for cat/student
+        cat_score.refresh_from_db()
+        self.assertEquals(3, cat_score.consecutive_correct)
+        self.assertEquals(5, cat_score.times_seen)
+        self.assertEquals(3, cat_score.times_correct)
+        self.assertEquals(self.tomorrow, cat_score.next_review)
+
+    def test_quiz_results_not_due_review_incorrect(self):
+        # cat_score is due for review tomorrow, so this is an optional extra revision session
+        # getting it incorrect DOES affect the WordScore
+        self.client.force_login(self.student)
+        cat_score = WordScore.objects.create(word=self.cat, student=self.student, times_correct=3, times_seen=5,
+                                             next_review=self.tomorrow, consecutive_correct=3)
+        quiz_results = {
+            '2': False
+        }
+        results = json.dumps(quiz_results)
+        self.client.post(self.path, {'results': results})
+
+        # check that the WordScore has changed for cat/student
+        cat_score.refresh_from_db()
+        self.assertEquals(3, cat_score.times_correct)
+        self.assertEquals(0, cat_score.consecutive_correct)
+        self.assertEquals(6, cat_score.times_seen)
+        self.assertEquals(self.tomorrow, cat_score.next_review)
 
 
 class QuizBuilderTests(TestCase):
