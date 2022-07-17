b9a535e0a7b052ac00e2979a8ef08b4218bd544e
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-07-17 12:11:02 +0100

    Rename student dashboard to 'Progress', new URLs

--- charts/urls.py
+++ charts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import dashboard, chart_topic_points, chart_topic_quizzes, chart_topic_words
+from .views import progress, chart_topic_points, chart_topic_quizzes, chart_topic_words, class_dashboard, \
+    get_filtered_chart_data
 
 urlpatterns = [
-    path('', dashboard, name='dashboard'),
-    path('data/topic-points', chart_topic_points, name='data-topic-points'),
-    path('data/topic-quizzes', chart_topic_quizzes, name='data-topic-quizzes'),
-    path('data/topic-words', chart_topic_words, name='data-topic-words'),
+    path('', progress, name='progress'),
+    path('class/', class_dashboard, name='dashboard'),
+    path('data/topic-points/', chart_topic_points, name='data-topic-points'),
+    path('data/topic-quizzes/', chart_topic_quizzes, name='data-topic-quizzes'),
+    path('data/topic-words/', chart_topic_words, name='data-topic-words'),
+    path('data/filter-chart/', get_filtered_chart_data, name='filter-chart'),
 ]
