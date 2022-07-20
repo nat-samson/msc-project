9d3fae98c9581cfb1cb2468a6a6e444b48b4f913
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-07-20 21:41:27 +0100

    Add more Chart Tools tests

--- charts/chart_tools.py
+++ charts/chart_tools.py
@@ -1,22 +1,22 @@
 # [(area-colour1, border-colour1), (area-colour2, border-colour2)... etc]
-CHART_COLOURS = [
+DEFAULT_CHART_PALETTE = (
     ('rgba(255, 99, 132, 0.5)', 'rgba(255, 99, 132, 1)'),
     ('rgba(54, 162, 235, 0.5)', 'rgba(54, 162, 235, 1)'),
     ('rgba(255, 206, 86, 0.5)', 'rgba(255, 206, 86, 1)'),
     ('rgba(75, 192, 192, 0.5)', 'rgba(75, 192, 192, 1)'),
     ('rgba(153, 102, 255, 0.5)', 'rgba(153, 102, 255, 1)'),
     ('rgba(255, 159, 64, 0.5)', 'rgba(255, 159, 64, 1)'),
-]
+)
 
 
-def get_colours(count):
+def get_colours(count, palette=DEFAULT_CHART_PALETTE):
     # cycle through the available colours in the palette
     colours = []
 
-    if len(CHART_COLOURS) > 0:
+    if len(palette) > 0:
         for i in range(count):
-            rolling_index = i % len(CHART_COLOURS)
-            colour = CHART_COLOURS[rolling_index]
+            rolling_index = i % len(palette)
+            colour = palette[rolling_index]
             colours.append((colour[0], colour[1]))
 
     return unzip(colours)
--- charts/tests.py
+++ charts/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse
 
-from charts.chart_tools import unzip
+from charts import chart_tools
+from charts.chart_tools import unzip, get_colours
 from users.models import User
 
 
@@ -52,7 +53,7 @@ class DashboardTests(TestCase):
 
 
 class ChartTools(SimpleTestCase):
-    def test_unzip(self):
+    def test_unzip_good_input(self):
         zipped_data = [(1, "a"), (2, "b"), (3, "c")]
         expected = [(1, 2, 3), ("a", "b", "c")]
         actual = unzip(zipped_data)
@@ -62,4 +63,36 @@ class ChartTools(SimpleTestCase):
         zipped_data = [(1, "a")]
         expected = [(1,), ("a",)]
         actual = unzip(zipped_data)
+        self.assertEquals(expected, actual)
+
+    def test_unzip_empty(self):
+        empty_zip = []
+        expected = []
+        actual = unzip(empty_zip)
+        self.assertEquals(expected, actual)
+
+    def test_get_zero_colours(self):
+        expected = []
+        actual = get_colours(0)
+        self.assertEquals(expected, actual)
+
+    def test_cycle_through_colours(self):
+        # get more colours than there are presets
+        n = len(chart_tools.DEFAULT_CHART_PALETTE)
+        colours = get_colours(5 * n)
+
+        # the nth colour should be identical to the 2*nth, 3*nth etc.
+        n_colour = colours[0][n]
+        n2_colour = colours[0][n * 2]
+        n3_colour = colours[0][n * 3]
+        n_plus_1_colour = colours[0][n + 1]
+        n2_plus_1_colour = colours[0][2 * n + 1]
+        self.assertEquals(n_colour, n2_colour)
+        self.assertEquals(n_colour, n3_colour)
+        self.assertEquals(n_plus_1_colour, n2_plus_1_colour)
+
+    def test_get_colours_when_no_presets(self):
+        empty_preset_palette = []
+        actual = get_colours(5, empty_preset_palette)
+        expected = []
         self.assertEquals(expected, actual)
