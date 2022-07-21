b8d2c8ec51113b841216a0376eac6860f145f4b2
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-07-21 22:39:45 +0100

    Refactor dashboard.js, improve documentation

--- charts/static/charts/js/dashboard.js
+++ charts/static/charts/js/dashboard.js
@@ -1,62 +1,72 @@
 /**
- * Updates the text displayed in multiple UI boxes with data from a single AJAX request.
- * @param {Element[]} boxes - An array of HTML Elements.
- * @param {String} url - A String representing a URL to use for the GET request.
+ * @file Common data-visualisation helper functions for use by dashboard and progress HTML templates.
+ * @author Nathaniel Samson
  */
-function updateDataBoxes(boxes, url) {
+
+/**
+ * Asynchronously retrieves data from the server and hands it to an Updater function to display.
+ *
+ * @param {(Chart[]|Chart|Element[])} target - An HTML element (or array of HTML elements) to be updated with new data.
+ * @param {String} url - A String representing a URL to use for the GET request.
+ * @param {Function} updaterFunc - A Function that updates the target with the retrieved data.
+ */
+function updateData(target, url, updaterFunc) {
     $.ajax({
         method: "GET",
         dataType: "json",
         url: url,
         success: (data) => {
-            // update all the data boxes
-            const dataArray = Object.values(data)
-            for (let i = 0; i < dataArray.length; i++) {
-                boxes[i].lastElementChild.innerText = dataArray[i]
-            }
+            updaterFunc(target, data);
         },
-        error: (error_data) => {
-            console.log(error_data)
+        failure: (error) => {
+            console.log("Failed to retrieve data from server.", error)
         }
     })
 }
 
-function updateChart(chart, url) {
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: url,
-        success: (chartData) => {
-            chart['data'] = chartData;
+/**
+ * Updates the innerText displayed in the 'box' elements in the Dashboard UI.
+ * @param {Element[]} boxes - An array of HTML Elements to be updated with new data.
+ * @param data - The raw data retrieved by updateData's asynchronous request.
+ */
+function boxesUpdater(boxes, data) {
+            // update all the boxes
+            const dataArray = Object.values(data);
+            for (let i = 0; i < dataArray.length; i++) {
+                boxes[i].lastElementChild.innerText = dataArray[i];
+            }
+}
+
+/**
+ * Updates the chart with data received by updateData.
+ * @param {Chart} chart - A Chart object to be updated with new data.
+ * @param data - The raw data retrieved by updateData's asynchronous request.
+ */
+function chartUpdater(chart, data) {
+            // update the given chart
+            chart['data'] = data;
             chart.update();
-        },
-        error: (error_data) => {
-            console.log("Failed to fetch chart data.")
-            console.log(error_data)
-        }
-    })
 }
 
-function updateCharts(charts, url) {
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: url,
-        success: (chartsData) => {
-            // update the charts
-            const dataArray = Object.values(chartsData);
-            for (let i = 0; i < dataArray.length; i++) {
-                charts[i]['data'] = dataArray[i]
-                charts[i].update();
-            }
-        },
-        error: (error_data) => {
-            console.log("Failed to fetch chart data.")
-            console.log(error_data)
-        }
-    })
+/**
+ * Updates each chart in the given array with data received by updateData.
+ * @param {Chart[]} chartsArray - An array of Chart objects to be updated with new data.
+ * @param data - The raw data retrieved by updateData's asynchronous request.
+ */
+function chartsUpdater(chartsArray, data) {
+    // update each chart in the array
+    const dataArray = Object.values(data);
+    for (let i = 0; i < dataArray.length; i++) {
+        chartsArray[i]['data'] = dataArray[i];
+        chartsArray[i].update();
+    }
 }
 
+/**
+ * Returns the template of common Chart settings for building the Chart objects.
+ * @param {String} chartType - A String representing the type of Chart to be created.
+ * @returns {{options: {}, type: {String}}} - An Object representing the settings for a Chart.
+ */
 function getChartSettings(chartType){
     let chartSettings =  {
         type: chartType,
