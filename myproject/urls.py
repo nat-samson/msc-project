669665d94319e10ca3bcd2bec5e1212b75428399
Nat S <64223008+nat-samson@users.noreply.github.com>
2022-09-04 16:36:38 +0100

    Add favicon, update requirements.txt

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('editor/', include('editor.urls')),
     path('', include('quizzes.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('favicon.ico/', RedirectView.as_view(url=staticfiles_storage.url('quizzes/img/favicon.ico'))),
 ]
